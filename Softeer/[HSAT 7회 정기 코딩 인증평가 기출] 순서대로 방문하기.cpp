fb57b5dc4b3a9305044b917a1cbddfc77103c1b3
ChoiYJ2 <chldb1221@naver.com>
2024-03-20 16:13:38 +0900

    Update [HSAT 7회 정기 코딩 인증평가 기출] 순서대로 방문하기.cpp
    Fail
    2024_03_20: Modify(BFS -> DFS)

--- Softeer/[HSAT 7회 정기 코딩 인증평가 기출] 순서대로 방문하기.cpp
+++ Softeer/[HSAT 7회 정기 코딩 인증평가 기출] 순서대로 방문하기.cpp
@@ -1,63 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int dy[] = {-1, 1, 0, 0};
-int dx[] = {0, 0, -1, 1};
 int n, m;
 int map[5][5];
 int visited[5][5];
 int cnt;
-
-struct Point{
+int dy[] = { -1, 1, 0, 0 };
+int dx[] = { 0, 0, -1, 1 };
+struct Point
+{
     int y;
     int x;
-    int flag;
 };
-
 vector<Point> goal;
 
-void bfs(int y, int x)
+void dfs(int lev, int y, int x)
 {
-    queue<Point> q;
-    q.push({y, x, 0});
-    visited[y][x] = 1;
-    while(!q.empty())
+    if (lev == m)
     {
-        Point now = q.front();
-        q.pop();
-        for(int i = 0; i < 4; i++)
-        {
-            int ny = now.y + dy[i];
-            int nx = now.x + dx[i];
-            if(ny < 1 || nx < 1 || ny > n || nx > n)
-                continue;
-            if(map[ny][nx] == 1)
-                continue;
-        }
+        if (y == goal[lev - 1].y && x == goal[lev - 1].x)
+            cnt++;
+        return;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 1 || nx < 1 || ny > n || nx > n)
+            continue;
+        if (map[ny][nx] || visited[ny][nx])
+            continue;
+        visited[ny][nx] = 1;
+        if (ny == goal[lev].y && nx == goal[lev].x)
+            dfs(lev + 1, ny, nx);
+        else
+            dfs(lev, ny, nx);
+        visited[ny][nx] = 0;
     }
 }
 
 int main()
 {
     cin >> n >> m;
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n; j++)
+        for (int j = 1; j <= n; j++)
             cin >> map[i][j];
     }
 
-    for(int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
         int y, x;
         cin >> y >> x;
-        goal.push_back({y, x, 0});
+        goal.push_back({ y, x });
     }
-
-    bfs(goal[0].y, goal[0].x);
-    
+    visited[goal[0].y][goal[0].x] = 1;
+    dfs(1, goal[0].y, goal[0].x);
     cout << cnt;
     return 0;
 }
