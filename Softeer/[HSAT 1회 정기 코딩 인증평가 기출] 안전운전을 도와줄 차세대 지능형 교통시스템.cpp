edf9f2904acba3afc445c3c9a3b79daf43fcb604
ChoiYJ2 <chldb1221@naver.com>
2024-03-21 15:43:37 +0900

    Update [HSAT 1회 정기 코딩 인증평가 기출] 안전운전을 도와줄 차세대 지능형 교통시스템.cpp
    Fail
    Subtask1: 1/10
    Subtask2: 3/30

--- Softeer/[HSAT 1회 정기 코딩 인증평가 기출] 안전운전을 도와줄 차세대 지능형 교통시스템.cpp
+++ Softeer/[HSAT 1회 정기 코딩 인증평가 기출] 안전운전을 도와줄 차세대 지능형 교통시스템.cpp
@@ -4,55 +4,56 @@
 using namespace std;
 
 int n, t;
-int trafficLight[101][101][4];
+int trafficSignal[101][101][4];
 int visited[101][101];
-int nowTime = 0;
-int moveCar;
-int dy[] = { 0, -1, 0, 1 };
-int dx[] = { 1, 0, -1, 0 };
+int moving;
+int dy[] = {0, -1, 0, 1, 0};
+int dx[] = {1, 0, -1, 0, 0};
+int signal[13][3] = {{4, 4, 4},
+{0, 1, 3}, {1, 0, 2}, {2, 3, 1}, {3, 2, 0},
+{0, 1, 4}, {1, 0, 4}, {2, 3, 4}, {3, 2, 4},
+{0, 4, 3}, {1, 4, 2}, {2, 4, 1}, {3, 4, 0}
+};
 
 struct Car
 {
     int y;
     int x;
-    int dir;
+    int direction;
+    int nowTime;
 };
 
-int trafficSignal[13][3] = { {0, 0, 0}, {0, 1, 3}, {1, 0, 2}, {2, 3, 1}, {3, 2, 0},
-                        {0, 1, 4}, {1, 0, 4}, {2, 3, 4}, {3, 2, 4},
-                        {0, 4, 3}, {1, 4, 2}, {2, 4, 1}, {3, 4, 0} };
-
-void bfs(int y, int x)
+void bfs()
 {
     queue<Car> q;
+    q.push({1, 1, 1, 0});
     visited[1][1] = 1;
-    q.push({ 1, 1, 2 });
-    while (!q.empty())
+    while(!q.empty())
     {
         Car now = q.front();
         q.pop();
-        int nowLightNum = trafficLight[now.y][now.x][nowTime];
-        //for(int i = 0; i < 4; i++)
-            //cout << trafficLight[now.y][now.x][i] << "\n";
-        cout << nowLightNum << "\n";
-        cout << now.dir << "\n";
-        if (now.dir != nowLightNum)
-            continue;
+        moving++;
+        if(now.nowTime > t)
+            return;
+        int nowLightSignal = trafficSignal[now.y][now.x][now.nowTime % 4];
+        if(now.direction != signal[nowLightSignal][0])
+            return;
 
-        int nowSignal[3];
-        for (int i = 0; i < 3; i++)
-            nowSignal[i] = trafficSignal[nowLightNum][i];
-        for (int i = 0; i < 3; i++)
+        int tmpSignal[3];
+        for(int i = 0; i < 3; i++)
+            tmpSignal[i] = signal[nowLightSignal][i];
+
+        for(int i = 0; i < 3; i++)
         {
-            int ny = now.y + dy[nowSignal[i]];
-            int nx = now.x + dx[nowSignal[i]];
+            int ny = now.y + dy[tmpSignal[i]];
+            int nx = now.x + dx[tmpSignal[i]];
 
-            if (ny < 1 || nx < 1 || ny > n || nx > n)
+            if(ny < 1 || nx < 1 || ny > n || nx > n)
                 continue;
-            if (visited[ny][nx])
+            if(visited[ny][nx])
                 continue;
-            moveCar++;
-            nowTime++;
+            visited[ny][nx] = 1;
+            q.push({ny, nx, tmpSignal[i], now.nowTime + 1});
         }
     }
 }
@@ -60,15 +61,17 @@ void bfs(int y, int x)
 int main()
 {
     cin >> n >> t;
-    for (int i = 1; i <= n; i++)
+    for(int i = 1; i <= n; i++)
     {
-        for (int k = 1; k <= n; k++)
+        for(int j = 1; j <= n; j++)
         {
-            for (int j = 0; j < 4; j++)
-                cin >> trafficLight[i][k][j];
+            for(int k = 0; k < 4; k++)
+                cin >> trafficSignal[i][j][k];
         }
     }
-    bfs(1, 1);
-    cout << moveCar;
+
+    bfs();
+
+    cout << moving;
     return 0;
 }
