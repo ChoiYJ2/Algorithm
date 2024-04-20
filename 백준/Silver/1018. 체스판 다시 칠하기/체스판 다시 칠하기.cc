7b09d6d9cad7779ee15970eb995dd431f486b5b2
ChoiYJ2 <88170337+ChoiYJ2@users.noreply.github.com>
2024-04-20 15:15:16 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 8 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -16,60 +16,22 @@ struct Coord
     int y, x;
 };
 
-void bfs(int y, int x)
+void bfs(int y, int x, int nowColor)
 {
     queue<Coord> q;
     q.push({ y, x });
     visited[y][x] = 1;
     int change = 0;
     char subBoard[51][51];
-    memset(subBoard, 0, sizeof(subBoard));
+    memset(subBoard, ' ', sizeof(subBoard));
     for (int i = y; i < y + 8; i++)
     {
         for (int j = x; j < x + 8; j++)
             subBoard[i][j] = board[i][j];
     }
-    while (!q.empty())
-    {
-        Coord now = q.front();
-        q.pop();
-        if (now.y >= n || now.x >= m)
-            break;
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = now.y + dy[i];
-            int nx = now.x + dx[i];
-            if (ny < y || nx < x || abs(ny - y) >= 8 || abs(nx - x) >= 8)
-                continue;
-            if (visited[ny][nx])
-                continue;
-            visited[ny][nx]++;
-            if (subBoard[ny][nx] == subBoard[now.y][now.x])
-            {
-                if (subBoard[now.y][now.x] == 'B')
-                    subBoard[ny][nx] = 'W';
-                else if (subBoard[now.y][now.x] == 'W')
-                    subBoard[ny][nx] = 'B';
-                change++;
-            }
-            q.push({ ny, nx });
-        }
-    }
-    minChange = min(minChange, change);
-
-    memset(subBoard, 0, sizeof(subBoard));
-    memset(visited, 0, sizeof(visited));
-    for (int i = y; i < y + 8; i++)
-    {
-        for (int j = x; j < x + 8; j++)
-            subBoard[i][j] = board[i][j];
-    }
-    change = 1;
-    if (subBoard[y][x] == 'B')
-        subBoard[y][x] = 'W';
-    else if (subBoard[y][x] == 'W')
-        subBoard[y][x] = 'B';
-    q.push({ y, x });
+    subBoard[y][x] = nowColor;
+    if (nowColor != board[y][x])
+        change++;
     while (!q.empty())
     {
         Coord now = q.front();
@@ -115,7 +77,12 @@ int main()
             if (y + 7 >= n || x + 7 >= m)
                 break;
             memset(visited, 0, sizeof(visited));
-            bfs(y, x);
+            bfs(y, x, board[y][x]);
+            memset(visited, 0, sizeof(visited));
+            if (board[y][x] == 'B')
+                bfs(y, x, 'W');
+            else
+                bfs(y, x, 'B');
         }
     }
 
